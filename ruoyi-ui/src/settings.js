81e69e51774fc81e1f9e38427d86f30a317ace98
東雲 <14447630+ur831143@user.noreply.gitee.com>
2026-07-31 12:00:00 +0800

    更新能源平台应用配置

--- ruoyi-ui/src/settings.js
+++ ruoyi-ui/src/settings.js
@@ -62,5 +62,5 @@ module.exports = {
   /**
    * 底部版权文本内容
    */
-  footerContent: 'Copyright © 2018-2026 RuoYi. All Rights Reserved.'
+  footerContent: 'Copyright © 2026 飞毛腿能源科技智能管理中心. All Rights Reserved.'
 }
